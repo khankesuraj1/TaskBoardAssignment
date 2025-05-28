3110730efd5b3639c57efecb91f01147a4672cf6
khankesuraj1 <160396375+khankesuraj1@users.noreply.github.com>
2025-05-28 14:16:18 +0530

    Create App.jsx
    added

---
+++ App.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import TaskBoard from "./components/TaskBoard";
+
+const App = () => {
+  const [tasks, setTasks] = useState(() => {
+    const stored = localStorage.getItem("taskboard-tasks");
+    return stored
+      ? JSON.parse(stored)
+      : {
+          todo: [],
+          inprogress: [],
+          done: [],
+        };
+  });
+
+  useEffect(() => {
+    localStorage.setItem("taskboard-tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>TaskBoard Pro</h1>
+      <TaskBoard tasks={tasks} setTasks={setTasks} />
+    </div>
+  );
+};
+
+export default App;
